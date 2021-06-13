4b696d847871971d503644fede469c551ecc6af6
nsyahirahsyahmi <82050487+nsyahirahsyahmi@users.noreply.github.com>
2021-06-13 20:38:08 +0800

    Add files via upload

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    function index()
+    {
+        $data= Product::all();
+        return view('product',['products'=>$data]);
+    }
+}
